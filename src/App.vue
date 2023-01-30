b9e2db69b87784275e8d101ab64e95a11ce843b5
LucasPerezDev <LucasPerezDev@gmail.com>
2023-01-30 16:21:16 -0500

    Laid the foundation for altering the background color of the application using three input tags that are linked to the style of the <main> tag using v-model and refs

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,31 @@
 <script setup>
+  import { ref } from 'vue';
 
+  const red = ref(0)
+  const green = ref(0)
+  const blue = ref(0)
 </script>
 
 <template>
-<main>
-  
+<main :style="{backgroundColor: `rgb(${red}, ${green}, ${blue})`}">
+  <div>
+    <input type="number" v-model="red" placeholder="red">
+    <input type="number" v-model="green" placeholder="green">
+    <input type="number" v-model="blue" placeholder="blue">
+  </div>
 </main>
 </template>
 
 <style scoped>
+  main {
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
 
+  div {
+    display: flex;
+    flex-direction: column;
+  }
 </style>
