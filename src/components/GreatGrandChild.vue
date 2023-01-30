3a378b50350e9506937151318d3e984f86a2859c
LucasPerezDev <LucasPerezDev@gmail.com>
2023-01-30 18:11:10 -0500

    Really leveled up my skills in Vue with the use of Pinia. Pinia allows for the use of state globally. This allows for a lot of useful different usecases, including the use of functions and being able to pass props as well as invoke those functions at the component you're needing them for. This is the most efficient and clean way to think about state and using state that I have learned with Vue so far

--- src/components/GreatGrandChild.vue
+++ src/components/GreatGrandChild.vue
@@ -1,13 +1,14 @@
 <script setup>
-  import useNumbers from '../composables/useNumbers';
+  import { useNumbersStore } from '../assets/stores/numbers';
+import useNumbers from '../composables/useNumbers';
 
-  const { numbers, filterNum } = useNumbers()
+  const { numbers, filterNum } = useNumbersStore()
 </script>
 
 <template>
   <div>
     <h1>
-      Great Grandchild Component {{ filterNum(5) }}
+      Great Grandchild Component {{ filterNum(17) }}
     </h1>
   </div>
 </template>
