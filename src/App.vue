3a63f4677777b4c41318e2c307babf9af167a573
LucasPerezDev <LucasPerezDev@gmail.com>
2023-01-30 16:57:45 -0500

    Created nested components to setup for the next lesson that covers props drilling

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,11 @@
 <script setup>
-  import { ref, reactive } from 'vue';
-
-  // const red = ref(0)
-  // const green = ref(0)
-  // const blue = ref(0)
-
-  // const color = ref({
-  //   red: 0,
-  //   green: 0,
-  //   blue: 0
-  // })
-
-  //NOTE: Ref can take any value whereas reactive only takes in objects. This can be used 
-  //as a point of distinction for when to use one or the other. 
-  //The other difference is to access values using ref, we utilize '.value' whereas when using
-  //reactive you can access state directly.
-  //The last difference is with ref you can update the entire object whereas with reactive you
-  //can only update properties.
-
-  const color = reactive({
-    red: 0,
-    green: 0,
-    blue: 0
-  })
-
-  const selectRandomColor = () => {
-    color.red = Math.floor(Math.random() * 255);
-    color.green = Math.floor(Math.random() * 255);
-    color.blue = Math.floor(Math.random() * 255);
-  }
+  import Parent from './components/Parent.vue'
 </script>
 
 <template>
-<main :style="{backgroundColor: `rgb(${color.red}, ${color.green}, ${color.blue})`}">
-  <div>
-    <input type="number" v-model="color.red" placeholder="red">
-    <input type="number" v-model="color.green" placeholder="green">
-    <input type="number" v-model="color.blue" placeholder="blue">
-    <button @click="selectRandomColor">Select Random Color</button>
-  </div>
-</main>
+  <main>
+    <Parent  />
+  </main>
 </template>
 
 <style scoped>
---
+++ src/components/GrandChild.vue
@@ -0,0 +1,13 @@
+<script setup> 
+  import GreatGrandChild from './GreatGrandChild.vue'
+</script>
+
+<template>
+  <div>
+    <h1>
+      Grandchild Component
+    </h1>
+    <div class="line"></div>
+    <GreatGrandChild  />
+  </div>
+</template>
